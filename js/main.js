ffd49f34bfd42e44bf2631884707eb68c28db463
David Keller <dckeller1@gmail.com>
2018-01-15 16:01:57 -0700

    Final commit, awaiting feedback from Sean

--- js/main.js
+++ js/main.js
@@ -1,6 +1,33 @@
 // Click Event Example //
 
-document.getElementsByClassName("fish-list-card-image").addEventListener("click", function(e) {
-		console.log("It works!!!")
+document.addEventListener("click", function() {
+	var wordmark = document.getElementById("wordmark")
+	var background = document.getElementById("body")
+	var heading = document.getElementById("main-heading")
+	var address = document.getElementsById("address")
+	console.log("Variables work!")
+		
+// This will change the title of the website //		
+		wordmerk.innerText = "NemosPals"
+		wordmark.style.color = "white"
+		wordmark.style.webkitTextStrokeColor = "black"
+		console.log("Title works!")
+
+		
+// This will change the background color //
+		background.style.background-color = "orange"
+		console.log("Background works!")
+		
+// This will change the main.heading //
+		heading.innerText = "Fish are Friends, not Food"
+		heading.style.color == "white"
+		console.log("Main Heading works!")
+
+// This will change the address //
+		address.innerText = "42 Wallaby Way, Sydney"
+		address.style.color = "black"
+		console.log("Everything works!")		
 });
+
+
 
